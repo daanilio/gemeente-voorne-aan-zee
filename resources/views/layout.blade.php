ee229f04affcb859d79cdce2142179446fadc4ec
Lisaa-vd <vandongenlisa@hotmail.com>
2023-12-05 09:59:50 +0100

    Create/store werkt. Zonder image

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <!-- Voeg Tailwind CSS toe -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+</head>
+
+<body class="bg-gray-100">
+
+<!-- Header -->
+<header class="py-3 border-b-2 border-gray bg-white px-4 md:px-20">
+    <div class="md:flex md:justify-between md:items-center text-black">
+        <img class="mx-auto p-2 w-48 md:w-52 px-2 md:px-4" alt="Logo van VaZ" src="{{ URL('img/logo_vaz_text.png') }}">
+        <nav class="lg:flex md:pt-0 pt-2 flex-grow md:justify-center md:items-center">
+            <ul class="flex space-x-8 justify-center md:justify-start">
+                <li><a href="#"
+                       class="text-lg md:text-xl p-2 transition duration-300 border-b-2 border-transparent hover:border-gray-500">Hoofdpagina</a>
+                </li>
+                <li><a href="#"
+                       class="text-lg md:text-xl p-2 transition duration-300 border-b-2 border-transparent hover:border-gray-500">Klacht
+                        indienen</a></li>
+                <li class="md:hidden">
+                    <a href="#"
+                       class="text-lg md:text-xl p-2 transition duration-300 border-b-2 border-transparent hover:border-gray-500">Inloggen</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="text-center hidden md:block">
+            <ul>
+                <li><a href="#"
+                       class="text-lg md:text-xl p-2 transition duration-300 border-b-2 border-transparent hover:border-gray-500">Inloggen</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+
+@yield('content')
+
+<!-- Footer -->
+<footer class="px-4 md:px-20 bg-white py-2 bottom-0 relative border-t-2 text-gray-500">
+    <div class="flex flex-col md:flex-row justify-between items-center">
+        <p class="mb-2 md:mb-0"><strong>Gemeente Voorne aan Zee</strong> klachten-applicatie</p>
+        <div class="flex items-center space-x-4 md:ml-4">
+            <p class="text-sm md:text-base mb-2 md:mb-0">Liever bellen? 06-12345678.</p>
+            <img src="{{ URL('img/logo_vaz.png') }}" alt="Foto van Voorne aan Zee" class="h-8 md:h-12 max-w-full">
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,97 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voorne aan Zee</title>
+@extends('layout')
 
-    <!-- Voeg Tailwind CSS toe -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+@section('title', 'VaZ klachten-applicatie')
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-
-</head>
-
-<body class="bg-gray-100">
-<!-- Header -->
-<header class="bg-white-500 text-white py-4 border border-b-2 border-gray bg-white">
-    <div class="container mx-auto flex justify-between items-center px-4 text-black px-96">
-        <img class="w-52 px-4" alt="Logo van VaZ" src="{{ URL('img/logo_vaz.png') }}">
-        <nav class="hidden lg:block">
-            <ul class="flex space-x-16">
-                <li><a href="#" class="text-xl hover:bg-blue-900 hover:text-white p-2">Home</a></li>
-                <li><a href="#" class="text-xl hover:bg-blue-900 hover:text-white p-2">Klacht indienen</a></li>
-                <li><a href="#" class="text-xl hover:bg-blue-900 hover:text-white p-2">Inloggen</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
-
-<section class="p-20">
-    <!-- Afbeelding -->
-    <div class="relative overflow-hidden">
-        <img src="{{ URL('img/vaz_strand.jpg') }}" alt="Foto van Voorne aan Zee" class="w-full h-auto rounded-lg" style="height: 500px">
-        <div class="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-30 bg-white text-white p-4 rounded">
-            <p class="text-4xl">Gemeente Voorne aan Zee</p>
-        </div>
-        <svg class="absolute bottom-0 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250">
-            <path fill="#F3F4F6"
-                  d="M0,192L48,202.7C96,213,192,235,288,213.3C384,192,480,128,576,112C672,96,768,128,864,160C960,192,1056,224,1152,213.3C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>
-    </div>
-
-
-    <!-- Formulier -->
-    <div class="w-full flex justify-center py-12">
-        <section class="container mx-auto flex justify-between w-3/4 bg-gray-100">
-            <div class="w-full md:w-1/2 p-4 mt-[-2rem] bg-white rounded-lg mr-8 border-gray-400 border-2">
-                <h2 class="font-bold text-2xl">Een klacht indienen</h2>
-                <br>
-                <p>Als u een klacht wilt indienen kan dat via dit formulier.
+@section('content')
+    <main class="p-4 md:p-20">
+        <section class="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div class="flex flex-col space-y-8">
+                <div class="bg-white shadow-lg p-4">
+                    <p class="text-2xl md:text-3xl font-bold">Gemeente VaZ klachten-applicatie</p>
                     <br>
-                    Uw informatie wordt niet opgeslagen of verstuurd. Het invullen is dus anoniem.
-                    <br><br>
-                    Mocht u een klacht willen invullen met uw naam erbij, kan dat. Log dan in en verstuur vervolgens een
-                    klacht via 'klacht indienen'.
-                </p>
-
+                    <p class="text-base md:text-m">Beste burger, u bevindt zich op de klachten-applicatie van Gemeente
+                        Voorne aan
+                        Zee.<br>
+                        Wilt u een klacht indienen? Dan kan dat hier.<br>
+                        Wij gaan zo snel mogelijk aan de slag met uw klacht, alvast bedankt voor het indienen.</p>
+                </div>
+                <!-- Afbeelding -->
+                <div class="relative overflow-hidden shadow-lg">
+                    <img src="{{ URL('img/vaz_strand.jpg') }}" alt="Foto van Voorne aan Zee"
+                         class="w-full h-full object-cover">
+                </div>
             </div>
-            <div class="w-full md:w-1/2 p-4 mt-[-2rem] bg-white rounded-lg ml-8 border-gray-400 border-2">
-                <form>
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2 " for="klacht">
-                            Omschrijving klacht:
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="klacht" type="text" placeholder="Omschrijf klacht">
-                    </div>
-                    <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="locatie">
-                            Locatie:
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="locatie" type="email" placeholder="Selecteer locatie">
-                    </div>
-                    <div class="flex items-center justify-center">
-                        <button
-                            class="w-full md:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button">
-                            Verstuur klacht
-                        </button>
-                    </div>
-                </form>
+
+            <!-- Formulier -->
+            <div class="w-full md:w-auto">
+                <div class="bg-white shadow-lg p-4">
+                    <form method="post" action="{{ route('klacht.store') }}">
+                        @csrf
+                        @method('post')
+                        <div class="mb-4">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="naam">
+                                Naam:
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="naam" name="naam" type="text" placeholder="Uw voor- en achternaam">
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                                Email:
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="email" name="email" type="email" placeholder="Uw email">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="klacht">
+                                Omschrijving klacht:
+                            </label>
+                            <textarea
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-24"
+                                id="klacht" name="klacht" placeholder="Omschrijf uw klacht"></textarea>
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="locatie">
+                                Locatie:
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="locatie" name="locatie" type="text" placeholder="Selecteer locatie">
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="foto">
+                                Foto toevoegen:
+                            </label>
+                            <div
+                                class="flex items-center justify-between border rounded py-2 px-3 bg-white shadow-sm">
+                                <input
+                                    class="appearance-none outline-none w-full text-gray-700 mr-3 py-1"
+                                    id="foto" name="foto" type="file" accept="image/*">
+                                {{--                            <button class="bg-gray-400 hover:bg-gray-600 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">--}}
+                                {{--                                Uploaden--}}
+                                {{--                            </button>--}}
+                            </div>
+                        </div>
+                        <div class="mb-6 hidden">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="datum">
+                                Datum:
+                            </label>
+                            <input
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="datum" name="datum" type="text" placeholder="Huidige datum" readonly value="{{date('d-m-Y')}}">
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <button
+                                class="md:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="submit">
+                                Verstuur klacht
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </section>
-    </div>
-</section>
-
-<!-- Footer -->
-<footer class="bg-white py-4 bottom-0 relative border border-t">
-    <div class="container mx-auto text-center">
-        <!-- Voeg footer-inhoud toe -->
-    </div>
-</footer>
-
-</body>
-</html>
+    </main>
+@endsection
